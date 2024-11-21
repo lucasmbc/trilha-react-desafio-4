aad7a72e990ed78b3aa548d17078bc04b1c4049c
lucasmbc <lucasmbc@hotmail.com>
2024-11-21 20:12:23 -0300

    Resolução do desafio

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,17 +1,21 @@
-import styled from 'styled-components';
-
+import styled from "styled-components";
 
 export const ButtonContainer = styled.button`
     width: 100%;
     height: 42px;
-    background-color: #81259D;
-    color: #FFF;
+    background-color: #81259d;
+    color: #fff;
 
-    border: 1px solid #81259D;
+    border: 1px solid #81259d;
     border-radius: 21px;
 
     &:hover {
         opacity: 0.6;
-        cursor:pointer;
+        cursor: pointer;
     }
-`
+
+    &:disabled {
+        opacity: 0.6;
+        cursor: default;
+    }
+`;
